3471bf2c10de0b40403f74aef8d3bb37a23126f7
dbsrkdud <yungayoung63@gmail.com>
2025-02-21 09:48:18 +0900

    Initial commit

---
+++ src/main/java/app/officedepot/springofficedepot/SpringOfficedepotApplication.java
@@ -0,0 +1,13 @@
+package app.officedepot.springofficedepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOfficedepotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOfficedepotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/officedepot/springofficedepot/SpringOfficedepotApplicationTests.java
@@ -0,0 +1,13 @@
+package app.officedepot.springofficedepot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOfficedepotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
